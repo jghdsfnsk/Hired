21981e6ec619e54f9fa27a5cc35fb48ede2cfb0f
Zeke <166648035+jghdsfnsk@users.noreply.github.com>
2024-12-04 03:40:40 +0000

    Donr

--- frontend/stockedjobs_vue/tailwind.config.js
+++ frontend/stockedjobs_vue/tailwind.config.js
@@ -10,8 +10,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
   ],  
 };
 
--- frontend/stockedjobs_vue/vue.config.js
+++ frontend/stockedjobs_vue/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    server: "https://scaling-trout-x5rg97x4jvqw29q.github.dev/",
+    client: {
+      webSocketURL: {
+        protocol: 'wss', // Use Secure WebSocket
+      },
+    },
+  }
 })
