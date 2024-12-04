7c6e4eef419aaa4e8501d16f654e37de5d689faf
Zeke <166648035+jghdsfnsk@users.noreply.github.com>
2024-12-04 02:02:32 +0000

    Added npm

---
+++ frontend/stockedjobs_vue/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],  
+};
+
+
