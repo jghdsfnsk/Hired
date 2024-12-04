7d76ec3b9d3230578cab104d4a723cefa5849fc1
Nobody <bunnza2020@gmail.com>
2024-12-03 23:10:16 -0600

    Maybe

--- frontend/stockedjobs/src/views/HomeView.vue
+++ frontend/stockedjobs/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="text-amber-600">Home</p>
   </div>
 </template>
 
--- frontend/stockedjobs/tailwind.config.js
+++ frontend/stockedjobs/tailwind.config.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography')
   ],
 };
